b0bb1e4b56bfa8102e44b8b4ab3b6e8d3942da2b
harolditoc <harold.chambilla@usil.pe>
2024-02-08 03:35:28 -0500

    Radio para la ubicacion Destino a color y mensaje si se encuentra o no en la empresa

--- assets/vue/controllers/Hello.vue
+++ assets/vue/controllers/Hello.vue
@@ -9,6 +9,8 @@
               <p v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</p>
               <p class="mb-1">Latitud: {{ latitude }}</p>
               <p class="mb-4">Longitud: {{ longitude }}</p>
+              <p class="">Distancia: {{ distancia }}</p>
+              <p class="">Asistencia: {{ asistencia }}</p>
               <div id="map" class="mb-4"></div>
             </div>
           </div>
@@ -19,11 +21,13 @@
 </template>
   
   <script setup>
-  import { ref, onMounted, computed } from 'vue';
+  import { ref, onMounted } from 'vue';
   
   const latitude = ref(0);
   const longitude = ref(0);
   const errorMessage = ref(null);
+  const distancia = ref(null)
+  const asistencia = ref("");
   
   onMounted(() => {
     if (!navigator.geolocation) {
@@ -37,7 +41,7 @@
         console.log('Ubicación del usuario obtenida: ', position);
 
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBKG625KcwDUXUIvO0x22JMGYMV7DMqd7Q&callback=initMap`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBKG625KcwDUXUIvO0x22JMGYMV7DMqd7Q&callback=initMap&libraries=geometry`;
         script.defer = true;
         script.async = true;
         script.onerror = () => {
@@ -54,13 +58,44 @@
     window.initMap = () => {
       console.log('latitud:', latitude.value);
       console.log('longitud:', longitude.value);
-      const ubiActual = { lat: latitude.value, lng: longitude.value }
+      const ubiActual = { lat: latitude.value, lng: longitude.value };
+      // const ubiDestino = { lat: -12.033055274005628, lng: -76.99143343148506 };
+      // const ubiDestino = { lat: -12.058597, lng: -76.964071 };
+      const ubiDestino = { lat: -12.035484, lng: -76.990205 };
       const mapOptions = {
         center: ubiActual,
         zoom: 18
       };
       const map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      const marker = new google.maps.Marker({ position: ubiActual, map: map, title: "Aqui estoy!" })
+      const marker = [ 
+        new google.maps.Marker({ position: ubiActual, map: map, title: "Aqui estoy!" }),
+        new google.maps.Marker({ position: ubiDestino, map: map })
+      ]
+      const radio = 300;
+      const circle = new google.maps.Circle({
+            strokeColor: '#8CD0FF',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#8CD0FF',
+            fillOpacity: 0.35,
+            map: map,
+            center: ubiDestino,
+            radius: radio
+        });
+      const dist = google.maps.geometry.spherical.computeDistanceBetween(ubiActual, ubiDestino);
+
+      
+      if (dist <= radio) {
+        alert("Si llego");
+        const text = "Si esta en la empresa";
+        asistencia.value = text;
+      } else {
+        alert("No llego");
+        const text = "No esta en la empresa";
+        asistencia.value = text;
+      }
+
+      distancia.value = dist;
       console.log("MARKER:", marker.title)
     };
 
