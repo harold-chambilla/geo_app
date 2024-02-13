03806d8619e2eedc29045e9d34de440bbd97015a
M4HR <huertasramosmanuelangel@gmail.com>
2024-02-13 14:50:45 -0500

    prueba de mac

--- src/Controller/ConfiguracionController.php
+++ src/Controller/ConfiguracionController.php
@@ -4,11 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 
 class ConfiguracionController extends AbstractController
 {
@@ -22,21 +18,7 @@ class ConfiguracionController extends AbstractController
     public function configuracionUsuario(): Response
     {
         $ip_cliente = $_SERVER['REMOTE_ADDR'];
-        $process = new Process(['arp', '-a', $ip_cliente]);
         $direccion_mac = null;
-        try {
-            // Ejecutar el proceso y obtener la salida
-            $process->mustRun();
-            $output = $process->getOutput();
-            
-            // Extraer la dirección MAC de la salida
-            preg_match('/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/', $output, $matches);
-            
-            // Devolver la dirección MAC encontrada
-            $direccion_mac = isset($matches[0]) ? $matches[0] : null;
-        } catch (ProcessFailedException $exception) {
-            // No hacer nada.
-        }
         return $this->render('configuracion/usuario.html.twig', [
             'ip_cliente' => $ip_cliente,
             'direccion_mac' => $direccion_mac,
