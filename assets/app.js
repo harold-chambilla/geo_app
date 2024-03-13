d25d7dc7ce0d3d992128f20967c3b68661001452
harolditoc <harold.chambilla@usil.pe>
2024-03-13 08:10:18 -0500

    Vista de marcado presentable terminado

--- assets/app.js
+++ assets/app.js
@@ -6,16 +6,35 @@ import { createPinia } from 'pinia';
 import App from "../assets/vue/controllers/App.vue";
 // import Salida from "../assets/vue/controllers/Salida.vue"
 import Salida from "../assets/vue/controllers/resultado/Resultado.vue";
+import Marcado from "../assets/vue/controllers/marcado/Marcado.vue";
+import Map from "../assets/vue/controllers/marcado/Map.vue";
+const googleMapsApiKey = 'AIzaSyBKG625KcwDUXUIvO0x22JMGYMV7DMqd7Q';
+const googleMapsScript = document.createElement('script');
+googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=geometry&callback=initMap`;
+googleMapsScript.async = true;
+googleMapsScript.defer = true;
+
+// Agregar el script al final del cuerpo del documento
+document.body.appendChild(googleMapsScript);
+
 
 const pinia = createPinia();
+const marcado = createApp(Marcado);
 const app = createApp(App);
 const salida = createApp(Salida);
+const map = createApp(Map);
 
 app.use(pinia);
 app.mount('#app');
 
 salida.use(pinia);
-salida.mount('#salida')
+salida.mount('#salida');
+
+marcado.use(pinia);
+marcado.mount('#marcado');
+
+map.use(pinia);
+map.mount('#mapa')
 /*
  * Welcome to your app's main JavaScript file!
  *
