f660cd9189b6298dffce65e685f9cebb9964b62f
Kais-Angel <benavides.angel1999@gmail.com>
2024-07-23 18:11:57 -0500

    23/07/2024 modificaciones

---
+++ src/Controller/Empresa/HorarioController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Controller\Empresa;
+
+use App\Entity\Colaborador;
+use App\Entity\Empresa;
+use App\Funciones\Empresa\EmpresaFunciones;
+use App\Repository\ColaboradorRepository;
+use App\Repository\EmpresaRepository;
+use Doctrine\ORM\Query\Expr\Func;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/empresa/Horario', name: 'app_empresa_Horario_')]
+class HorarioController extends AbstractController 
+{	
+
+	#[Route('/', name: 'mostrar')]
+	public function mostrar(): Response
+	{
+		return $this->render('empresa/Horario/index.html.twig');
+	}
+
+}
