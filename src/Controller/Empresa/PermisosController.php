b545758cc34cb5a924336c01976314f7ca6b0c25
kais.Angel <benavides.angel1999@gmail.com>
2024-08-12 23:50:48 -0500

    front-end 12/08/2024

---
+++ src/Controller/Empresa/PermisosController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Controller\Empresa;
+
+use App\Entity\Colaborador;
+use App\Entity\Empresa;
+use App\Funciones\Empresa\EmpresaFunciones;
+use App\Repository\ColaboradorRepository;
+use App\Repository\EmpresaRepository;
+use Doctrine\ORM\Query\Expr\Func;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/empresa/Permisos', name: 'app_empresa_Permisos_')]
+class PermisosController extends AbstractController 
+{	
+
+	#[Route('/', name: 'mostrar')]
+	public function mostrar(): Response
+	{
+		return $this->render('empresa/Permisos/index.html.twig');
+	}
+
+}
