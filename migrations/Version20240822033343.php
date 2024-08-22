b515c7d60d178a783affaf0d6276f777bb35da5e
kais.Angel <benavides.angel1999@gmail.com>
2024-08-21 22:52:38 -0500

    frontend 21/08/2024

--- migrations/Version20240822033343.php
+++ migrations/Version20240822033343.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240820235322 extends AbstractMigration
+final class Version20240822033343 extends AbstractMigration
 {
     public function getDescription(): string
     {
