e9eed0c5f6082d2facf40836bb098febcff1cf01
M4HR <huertasramosmanuelangel@gmail.com>
2024-11-27 09:35:22 -0500

    Actualizacion de dependencias

--- assets/vue/controllers/empresa/horario/horario.vue
+++ assets/vue/controllers/empresa/horario/horario.vue
@@ -1,3 +1,38 @@
-<template></template>
-<script setup></script>
+<script>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import bootstrapPlugin from '@fullcalendar/bootstrap5';
 
+export default {
+  name: 'CalendarComponent',
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin, bootstrapPlugin],
+        themeSystem: 'bootstrap5',
+        initialView: 'dayGridMonth',
+        editable: true,
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,dayGridWeek'
+        },
+        events: [
+          { title: 'Evento 1', start: '2024-11-10' },
+          { title: 'Evento 2', start: '2024-11-12', end: '2024-11-15' }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<template>
+  <div class="container mt-4">
+    <FullCalendar :options="calendarOptions" />
+  </div>
+</template>
