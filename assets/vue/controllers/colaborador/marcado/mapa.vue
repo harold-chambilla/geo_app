e863daf68355ec461c40020e8cd0e774f6741b03
M4HR <56637687+M4HR@users.noreply.github.com>
2025-01-22 13:21:52 -0500

    progreso

--- assets/vue/controllers/colaborador/marcado/mapa.vue
+++ assets/vue/controllers/colaborador/marcado/mapa.vue
@@ -2,14 +2,21 @@
     <div id="map" class="w-100" style="height: 300px;"></div>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useMarcadoStore } from '@/store/colaborador/marcado';
 
 const marcadoStore = useMarcadoStore();
 
+const sede = computed(() => {
+  return marcadoStore.sede;
+});
+
 const latitude = ref(0);
 const longitude = ref(0);
 const exactitud = ref(0);
+const mapInitialized = ref(false);
+const verId = ref(null);
+const watchId = ref(null);
 
 const watchCoordinates = () => {
   return new Promise((resolve, reject) => {
@@ -28,23 +35,67 @@ const watchCoordinates = () => {
   });
 };
 
-// Mapa
-let map;
-
-const initMap = () => {
-    const mapOptions = {
-      center: { lat: -12.0464, lng: -77.0428 }, // Centro predeterminado (Lima, Perú)
-      zoom: 15,
-    };
-  
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  
-    // Agregar un marcador
-    new google.maps.Marker({
-      position: mapOptions.center,
-      map,
-      title: "Tu posición",
+const initMap = (latitud, longitud, exact, sedes) => {
+  if (!mapInitialized.value){
+    const ubiActual = { lat: latitud, lng: longitud };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
     });
+  
+    const usuarioMarker = new google.maps.Marker({
+      position: ubiActual,
+      map: map,
+      title: "Aqui estoy!"
+    });
+
+    new google.maps.Circle({
+      strokeColor: '#8CD0FF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#8CD0FF',
+      fillOpacity: 0.35,
+      map: map,
+      center: ubiActual,
+      radius: 300
+    });
+
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(ubiActual);
+
+    sedes.forEach((sede) => {
+      const ubicacionSede = { lat: parseFloat(sede.latitud), lng: parseFloat(sede.longitud) };
+
+      if (!isNaN(ubicacionSede.lat) && !isNaN(ubicacionSede.lng)) {
+        new google.maps.Marker({
+          position: ubicacionSede,
+          map: map,
+          title: sede.nombre
+        });
+
+        new google.maps.Circle({
+          strokeColor: '#FF5733',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FFC300',
+          fillOpacity: 0.35,
+          map: map,
+          center: ubicacionSede,
+          radius: sede.radio
+        });
+
+        bounds.extend(ubicacionSede);
+      }
+    });
+
+    map.fitBounds(bounds);
+
+    const tiles = new google.maps.event.addListenerOnce(map, 'tilesloaded', () => {
+      if (!mapInitialized.value) {
+        mapInitialized.value = true;
+        navigator.geolocation.clearWatch(watchId.value);
+      }
+    });
+  }
 };
 
 onMounted(() => {
@@ -57,19 +108,15 @@ onMounted(() => {
             latitude.value = coords.longitude;
             exactitud.value = coords.accuracy;
             console.log("Initializing map with coordinates:", latitude.value, longitude.value);
+
+            await marcadoStore.fetchSede(1); // Id de colaborador
+            console.log('sede: ', sede.value);
           } catch (error) {
             console.error('Error al obtener las coordenadas o sedes:', error);
           }
         };
+        cordenadasInitMap();
       }, 500);
     }
-
-    // Cargar el mapa
-    const googleMapsScript = document.createElement("script");
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&callback=initMap`;
-    googleMapsScript.async = true;
-    googleMapsScript.defer = true;
-    window.initMap = initMap; // Vincular la función initMap
-    document.head.appendChild(googleMapsScript);
 });
 </script>
