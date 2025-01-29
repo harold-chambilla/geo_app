085d2d9b1070a04bddc027e5d97fa7d7dc7d84d0
M4HR <56637687+M4HR@users.noreply.github.com>
2025-01-29 13:56:28 -0500

    Marcacion completa

--- assets/vue/controllers/colaborador/marcado/botones.vue
+++ assets/vue/controllers/colaborador/marcado/botones.vue
@@ -8,10 +8,56 @@
             SALIDA
         </button>
     </div>
+
+    <!-- Modal de Confirmación -->
+    <div class="modal fade" id="confirmacionModal" tabindex="-1" aria-labelledby="confirmacionModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content text-center p-4">
+
+          <!-- Sección del Icono -->
+          <div class="d-flex justify-content-center">
+            <img :src="iconoEstado" :alt="tituloModal" class="img-fluid rounded-circle border p-2 bg-light" style="width: 100px;">
+          </div>
+
+          <!-- Estado de Asistencia -->
+          <h5 class="fw-bold mt-3" :class="colorEstado">{{ tituloModal }}</h5>
+          <p class="text-muted">{{ mensajeModal }}</p>
+
+          <!-- Información de Entrada y Salida -->
+          <div class="list-group list-group-flush border rounded-3 shadow-sm">
+            <div class="list-group-item">
+              <i class="bi bi-calendar-event text-primary"></i> <strong>Fecha:</strong> {{ asistenciaData.asi_fechaentrada || asistenciaData.asi_fechasalida }}
+            </div>
+            <div class="list-group-item">
+              <i class="bi bi-clock text-primary"></i> <strong>Hora:</strong> {{ asistenciaData.asi_horaentrada || asistenciaData.asi_horasalida }}
+            </div>
+            <div v-if="(textoUbicacion && textoUbicacion !== 'No corresponde') || 
+                        (asistenciaData.asi_ubicacionentrada && asistenciaData.asi_ubicacionentrada !== 'No corresponde') || 
+                        (asistenciaData.asi_ubicacionsalida && asistenciaData.asi_ubicacionsalida !== 'No corresponde')"
+                class="list-group-item text-wrap text-break">
+              <i class="bi bi-geo-alt text-danger"></i> <strong>Ubicación:</strong> 
+              <span class="d-block text-justify">{{ textoUbicacion }}</span>
+            </div>
+          </div>
+
+          <!-- Botón de Cierre -->
+          <div class="modal-footer border-0 d-flex justify-content-center">
+            <button type="button" class="btn btn-primary px-4 py-2" data-bs-dismiss="modal">
+              <i class="bi bi-check-circle"></i> Aceptar
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
 </template>
 <script setup>
+import { Modal } from "bootstrap";
 import { useMarcadoStore } from '@/store/colaborador/marcado';
 import { ref, onMounted, computed, watch, onUnmounted } from "vue";
+import puntualidad from "@img/colaborador/ico_puntualidad.png";
+import tardanza from "@img/colaborador/ico_tardanza.png";
+import accesoDenegado from "@img/colaborador/ico_denegado.png";
 
 const marcadoStore = useMarcadoStore();
 
@@ -22,6 +68,11 @@ const distanciaSede = computed(() => { return marcadoStore.distanciaSede; })
 const asistencias = computed(() => { return marcadoStore.asistencias; })
 const horaActual = computed(() => { return marcadoStore.horaActual; })
 const exactitudRadio = ref(null);
+
+const tituloModal = ref("");
+const iconoEstado = ref("");
+const colorEstado = ref("");
+const textoUbicacion = ref("");
 
 const asistenciaData = ref({
   colaborador_id: null,
@@ -73,16 +124,29 @@ const marcarEntrada = async () => {
     const horaActualAux = new Date(`1970-01-01T${asistenciaData.value.asi_horaentrada}:00`);
     const toleranciaMinutos = horarioColaborador.value.configuracion_asistencia.cas_tolerancia_ingreso_minutos;
     const horaTolerancia = new Date(horaEntrada.getTime() + toleranciaMinutos * 60 * 1000);
+    textoUbicacion.value = ubicacion.value.latitud && ubicacion.value.longitud ? `Lat: ${ubicacion.value.latitud}, Lng: ${ubicacion.value.longitud}` : 'Ubicación no disponible.';
 
     if (horaActualAux <= horaTolerancia) {
       asistenciaData.value.asi_estadoentrada = 'puntual';
+      tituloModal.value = "Puntual";
+      colorEstado.value = "text-success";
+      iconoEstado.value = puntualidad;
     } else {
       asistenciaData.value.asi_estadoentrada = 'tardanza';
+      tituloModal.value = "Tardanza";
+      colorEstado.value = "text-warning";
+      iconoEstado.value = tardanza;
     }
 
     if (horarioColaborador.value.horario.hot_tipojornada === 'presencial') {
       if (!distanciaSede.value.dentroRadio) {
+        tituloModal.value = "Acceso Denegado";
+        colorEstado.value = "text-danger";
+        textoUbicacion.value = "No estás en la ubicación requerida 📍❌";
+        iconoEstado.value = accesoDenegado;
         console.error('El colaborador no está dentro del lugar requerido para jornada presencial.');
+        const modal = new Modal(document.getElementById("confirmacionModal"));
+        modal.show();
         return;
       }
       asistenciaData.value.asi_fotoentrada = 'No configurado';
@@ -95,8 +159,17 @@ const marcarEntrada = async () => {
     asistenciaData.value.asi_notas = 'Entrada registrada correctamente';
 
     await marcadoStore.crearAsistencia(asistenciaData.value);
+
+    const modal = new Modal(document.getElementById("confirmacionModal"));
+    modal.show();
   } catch (err) {
+    tituloModal.value = "Error";
+    colorEstado.value = "text-danger";
+    iconoEstado.value = accesoDenegado;
+    textoUbicacion.value = "Hubo un problema al registrar la asistencia ❌";
     console.error('Error al registrar la asistencia:', err.response?.data || err.message);
+    const modal = new Modal(document.getElementById("confirmacionModal"));
+    modal.show();
   }
 };
 
@@ -116,7 +189,13 @@ const marcarSalida = async () => {
     }
 
     if (horarioColaborador.value.horario.hot_tipojornada === 'presencial' && !distanciaSede.value.dentroRadio) {
+      tituloModal.value = "Acceso Denegado";
+      colorEstado.value = "text-danger";
+      textoUbicacion.value = "No estás en la ubicación requerida 📍❌";
+      iconoEstado.value = accesoDenegado;
       console.error("El colaborador no está dentro del lugar requerido para cerrar su asistencia en una jornada presencial.");
+      const modal = new Modal(document.getElementById("confirmacionModal"));
+      modal.show();
       return;
     }
 
@@ -124,6 +203,9 @@ const marcarSalida = async () => {
     const horaSalida = convertirHoraA24(horaActual.value);
     const horaSalidaPermitida = new Date(`1970-01-01T${horarioColaborador.value.horario.hot_salida}:00`);
     const estadoSalida = ahora <= horaSalidaPermitida ? 'a_tiempo' : 'sobre_hora';
+    textoUbicacion.value = ubicacion.value.latitud && ubicacion.value.longitud
+      ? `Lat: ${ubicacion.value.latitud}, Lng: ${ubicacion.value.longitud}`
+      : 'Ubicación no disponible.';
 
     const asistenciaUpdate = {  
       asi_fechasalida: horarioColaborador.value.horario.hot_fecha,
@@ -131,15 +213,42 @@ const marcarSalida = async () => {
       asi_estadosalida: estadoSalida,
     };
 
+    Object.assign(asistenciaData.value, {  
+      asi_fechasalida: horarioColaborador.value.horario.hot_fecha,
+      asi_horasalida: horaSalida,
+      asi_estadosalida: estadoSalida,
+    });
+
+    if (estadoSalida === 'a_tiempo') {
+      tituloModal.value = "Salida a Tiempo";
+      colorEstado.value = "text-primary";
+      iconoEstado.value = puntualidad;
+    } else {
+      tituloModal.value = "Salida Sobre la Hora";
+      colorEstado.value = "text-warning";
+      iconoEstado.value = tardanza;
+    }
+
     if (horarioColaborador.value.horario.hot_tipojornada === 'presencial') {
       asistenciaUpdate.asi_fotosalida = 'No configurado';
       asistenciaUpdate.asi_ubicacionsalida = JSON.stringify(ubicacion.value);
+      Object.assign(asistenciaData.value, {
+        asi_fotosalida: 'No configurado',
+        asi_ubicacionsalida: JSON.stringify(ubicacion.value),
+      });
     } else if (horarioColaborador.value.horario.hot_tipojornada === 'remoto') {
       asistenciaUpdate.asi_fotosalida = 'No corresponde';
       asistenciaUpdate.asi_ubicacionsalida = 'No corresponde';
+      Object.assign(asistenciaData.value, {
+        asi_fotosalida: 'No corresponde',
+        asi_ubicacionsalida: 'No corresponde',
+      });
     }
 
     await marcadoStore.actualizarAsistencia(asistencia.id, asistenciaUpdate);
+
+    const modal = new Modal(document.getElementById("confirmacionModal"));
+    modal.show();
   } catch (err) {
     console.error('Error al registrar la salida:', err.response?.data || err.message);
   }
--- migrations/Version20250129181845.php
+++ migrations/Version20250129181845.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250128225926 extends AbstractMigration
+final class Version20250129181845 extends AbstractMigration
 {
     public function getDescription(): string
     {
